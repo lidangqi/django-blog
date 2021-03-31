49cf3cbf7c5a6c4863f1c89799b37cb4cf203e28
lidangqi <1074080451@qq.com>
2021-04-01 03:17:06 +0800

    docker部署

---
+++ blogproject/settings/local.py
@@ -0,0 +1,9 @@
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '%k^!^6-(z^+l*)i6-qg*el%5*2-@%+jx)jktqb!5t+j$5q1!9&'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
