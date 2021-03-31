ead0f314d6429242bf35505446ef1203cf657b61
lidangqi <1074080451@qq.com>
2021-04-01 02:53:18 +0800

    docker部署

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -7,4 +7,4 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ['DJANGO_DOMAINNAME_KEY']
+ALLOWED_HOSTS = os.environ['DJANGO_DOMAINNAME_KEY', '127.0.0.1', 'localhost']
