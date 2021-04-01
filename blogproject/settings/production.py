74d52dc505086ddfbad1357c1d48d0019bd65d6d
lidangqi <1074080451@qq.com>
2021-04-01 12:56:20 +0800

    docker部署

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -5,6 +5,6 @@ import os
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ['DJANGO_DOMAINNAME_KEY']
+ALLOWED_HOSTS = ['127.0.0.1', 'blog.lidangqi.com']
