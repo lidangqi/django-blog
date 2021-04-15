638d3b0026e12d99f8f77d97c8d04b79ba895609
lidangqi <1074080451@qq.com>
2021-04-15 21:03:00 +0800

    阿里云OSS

--- blogproject/settings/common.py
+++ blogproject/settings/common.py
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
